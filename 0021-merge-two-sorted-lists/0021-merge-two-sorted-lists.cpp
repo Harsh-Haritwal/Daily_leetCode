877d62ed3db8d8b1835f381e7a4e217a3ad3dbbd
Harsh-Haritwal <harshharitwal0321@gmail.com>
2026-08-31 10:31:00 +0530

    Time: 0 ms (100%), Space: 19.5 MB (27.66%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -10,6 +10,37 @@
  */
 class Solution {
 public:
+    ListNode* rec(ListNode* &temp1, ListNode* &temp2, ListNode* &ansList, ListNode* &temp3) {
+        if(temp1 == nullptr){
+            ansList->next = temp2;
+            return temp3;
+        }else if (temp2 == nullptr){
+            ansList->next = temp1;
+            return temp3;
+        }
+        if (temp1->val <= temp2->val) {
+            if (ansList == nullptr) {
+                ansList = temp1;
+                temp3 = ansList;
+            } else {
+                ansList->next = temp1;
+                ansList = ansList->next;
+            }
+            temp1 = temp1->next;
+        } else {
+            if (ansList == nullptr) {
+                ansList = temp2;
+                temp3 = ansList;
+            } else {
+                ansList->next = temp2;
+                ansList = ansList->next;
+            }
+            temp2 = temp2->next;
+        }
+        rec(temp1, temp2, ansList, temp3);
+        return temp3;
+    }
+
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         if (list1 == nullptr && list2 == nullptr)
             return list1;
@@ -21,33 +52,7 @@ public:
         ListNode* temp1 = list1;
         ListNode* temp2 = list2;
         ListNode* temp3;
-        while (temp1 != nullptr && temp2 != nullptr) {
-            if (temp1->val <= temp2->val) {
-                if (ansList == nullptr) {
-                    ansList = temp1;
-                    temp3 = ansList;
-                } else {
-                    ansList->next = temp1;
-                    ansList = ansList->next;
-                }
-                temp1 = temp1->next;
-            } else {
-                if (ansList == nullptr) {
-                    ansList = temp2;
-                    temp3 = ansList;
-                } else {
-                    ansList->next = temp2;
-                    ansList = ansList->next;
-                }
-                temp2 = temp2->next;
-            }
-        }
-        if (temp1 != nullptr) {
-            ansList->next = temp1;
-        } else if (temp2 != nullptr) {
-            ansList->next = temp2;
-        }
-
+        rec(temp1, temp2, ansList, temp3);
         return temp3;
     }
 };
