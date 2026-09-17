82346ede48eb7b6dd10a57937356ed1485f71a30
Harsh-Haritwal <harshharitwal0321@gmail.com>
2026-09-17 16:39:00 +0530

    Time: 15 ms (5.8%), Space: 12.3 MB (85.68%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int> st;
+        vector<int> nge;
         vector<int> ans;
-        for(int i =0 ;i <nums1.size();i++){
-            int temp;
-            for(int j = 0; i<nums2.size();j++){
-                if(nums2[j] == nums1[i]){
-                    temp = j;
-                    break;
-                }
+        for(int i =nums2.size()-1;i >=0;i--){
+            while(!st.empty() && !(nums2[i] < st.top())){
+                st.pop();
             }
-            int count = 0;
-            for(int k = temp+1; k<nums2.size();k++){
-                if(nums2[k] > nums1[i]){
-                    ans.push_back(nums2[k]);
-                    count++;
-                    break;
+            if(st.empty()) nge.push_back(-1);
+            else nge.push_back(st.top());
+            st.push(nums2[i]);
+        }
+        for(int i=0;i<nums1.size();i++){
+            for(int j = 0; j<nums2.size();j++){
+                if(nums1[i] == nums2[j]){
+                    ans.push_back(nge[nums2.size()-1-j]);
                 }
-            }
-            if(count == 0 ){
-                ans.push_back(-1);
             }
         }
         return ans;
